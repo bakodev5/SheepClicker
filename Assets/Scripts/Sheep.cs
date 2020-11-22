60189dde9f78e53c713391511a5c1acc9f8e14c7
bakodev5 <bakodev5@gmail.com>
2020-11-22 23:00:51 +0900

    講座第三回まで終了

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -7,6 +7,7 @@ public class Sheep : MonoBehaviour
 
     [SerializeField] private SpriteRenderer sheepRenderer;
     [SerializeField] private Sprite cutSheepSprite;
+    [SerializeField] private Wool woolPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,7 @@ public class Sheep : MonoBehaviour
     private void Shaving()
     {
         sheepRenderer.sprite = cutSheepSprite;
+        var wool = Instantiate(woolPrefab, transform.position, transform.rotation);
     }
 
     private void OnMouseOver()
