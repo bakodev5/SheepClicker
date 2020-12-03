19db437a62b5ddd74c20e9bcd040dba03ba754bc
bakodev5 <bakodev5@gmail.com>
2020-12-03 15:57:43 +0900

    講座第4回まで完了

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMain : MonoBehaviour
+{
+    [SerializeField] private Button sellButton;
+    [SerializeField] private Wallet wallet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sellButton.onClick.AddListener(SellAllWool);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SellAllWool()
+    {
+        var wools = FindObjectsOfType<Wool>(); //画面上の全てのWoolスクリプトが付いたオブジェクトを検索してWool配列woolsに入れる
+        foreach(var wool in wools)
+        {
+            Destroy(wool.gameObject);
+            wallet.money += 100; //TODO本当は羊毛ごとに金額を変えたい
+        }
+
+    }
+}
